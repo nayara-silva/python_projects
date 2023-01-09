c084d5e1e215e0cd17dc8efa658b0a977929403f
Nayara Silva <nayalink@yahoo.com.br>
2023-01-09 16:28:23 -0300

    add comments on project 9

---
+++ 9.secret_auction.py
@@ -0,0 +1,44 @@
+import os
+
+bid_list = {}
+end_auction = False
+
+def check_winner(bids):
+    highest_bid = 0
+    winner = ""
+    
+    for bidder in bids:
+        bid_value = bids[bidder]
+        if bid_value > highest_bid:
+            highest_bid = bid_value
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
+    
+def screen_clear():
+    #For Mac and Linux
+    if os.name == "posix":
+        _ = os.system("clear")
+    #For Windows
+    else:
+        _ = os.system("cls")
+    
+    
+while end_auction == False:
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? $"))
+    
+    bid_list[name] = bid
+    
+    more_bidders = input("Are there any other bidders? Tipe 'yes' or no'. ").lower()
+    screen_clear()
+    
+    if more_bidders == 'no':
+        check_winner(bid_list)
+        end_auction = True
+        
+    
+    
+        
+
+        
+
